b7ee81aa63db27b37398e00dee62d8702e295401
areatecnicabiofix <areatecnicabiofix@gmail.com>
2026-08-11 12:25:26 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // Conexión compartida con el Web App único de Apps Script
 // (stock + Panel + Pedidos, todo detrás del mismo doGet)
-var URL_API = 'https://script.google.com/macros/s/AKfycbzWd-Vj9O2E_fIBnrsaAK9GDfyFBw4DoYP3cCsTBpaZq-s5vsW-gQqxk_sVNc0hNZ3M/exec';
+var URL_API = 'https://script.google.com/macros/s/AKfycbxGJvUZhezyeYKFQk0kvSNlb1VPDV0tiSZ0UVnmtpWSZkn6E9ygcOSuJb0ozDcPimGI/exec';
 
 function jsonp(params, cb) {
   var cbName = 'jscb_' + Date.now() + '_' + Math.floor(Math.random() * 1000);
